eab080e32d5900ccce6d58cf2cbc8b0533f7cc1b
Joshua Trimm <trimmj@etsu.edu>
2022-02-05 00:07:05 -0500

    Health Attributes and Counties Populating

--- CPH/wwwroot/js/charts.js
+++ CPH/wwwroot/js/charts.js
@@ -86,7 +86,7 @@ const Chart = {
         this.setYScale
 
     },
-    createTheBottomAxisfunction() {
+    createTheBottomAxis() {
         var svg = this.getSvg;
 
         svg.append('g')
@@ -101,7 +101,6 @@ const Chart = {
     },
     buildTickSpacingArray() {  
         return [0, (this.width * 0.05), (this.width * 0.10), (this.width * 0.15), (this.width * 0.20), (this.width * 0.25), (this.width * 0.30), (this.width * 0.35), (this.width * 0.40), (this.width * 0.45), (this.width * 0.50), (this.width * 0.55), (this.width * 0.60), (this.width * 0.65), (this.width * 0.70), (this.width * 0.75), (this.width * 0.80), (this.width * 0.85), (this.width * 0.90), (this.width * 0.95), this.width];
-
     },
     defineTheAreaToDisplayChart(chartAreaDiv) {
         this.setSvg = chartAreaDiv;
@@ -142,5 +141,146 @@ const Chart = {
         });
     }
 }
+
+const ChartAttributes = new Vue({
+    el: '#Chart',
+    data: {
+        countiesDiv: document.getElementById('Counties'),
+        chartName: null,
+        dataHolder: null,
+        maxValue: 0,
+        minValue: 0,
+        year: 0,
+        healthAttribute: null,
+        healthAttributeData: [],
+        selectedCounties: []
+    },
+    methods: {
+        setChartAttributes(year) {
+            this.year = year.target.value;
+            //Get the columns div
+            let healthAttrs = document.getElementById("HealthAttrs");
+            let countiesDiv = document.getElementById("Counties");
+
+            let checkHealthAttrs = this.checkIfNodeIsEmpty(healthAttrs);
+            let checkCounties = this.checkIfNodeIsEmpty(countiesDiv);
+
+            if (checkCounties === false) {
+                this.removeAllChildNodes(countiesDiv);
+            }
+
+            if (checkHealthAttrs === false) {
+                this.removeAllChildNodes(healthAttrs);
+            }
+
+            d3.csv(`../uploads/${year.target.value}.csv`).then((data) => {
+ 
+                let counties = this.getCountyList(data);
+
+                //Place the csv data into a holder for later consumption 
+               // this.dataHolder = data;
+
+                this.addDataToUL(data.columns, healthAttrs, "radio"); // data.columns are the health attributes from the csv file.
+
+                this.addDataToUL(counties, countiesDiv);
+            });
+        },
+        addDataToUL(data, ulId, inputType = "checkbox") {
+            for (let i = 0; i < data.length; i++) {
+
+                //Create list item for the input and label to be inserted into
+                let liNode = document.createElement("li");
+
+                liNode.classList = ["form-check"];
+
+                //Create input node
+                let nodeInput = document.createElement("input");
+
+                nodeInput.type = inputType;
+                nodeInput.value = data[i];
+                nodeInput.id = data[i];
+                nodeInput.classList = ["form-check-input"];
+                nodeInput.name = ulId;
+
+                //Label for the checkboxes
+                let label = document.createElement('label');
+
+                label.htmlFor = data[i];
+
+                // append the created text to the created label tag
+                label.appendChild(document.createTextNode(`${data[i]}`));
+
+                // append the li to the ul div
+                ulId.appendChild(liNode);
+
+                // append the checkbox and label to the li's
+                liNode.appendChild(nodeInput);
+                liNode.appendChild(label);
+
+
+            }
+        },
+        getCountyList(data) {
+            var listOfCounties = [];
+            for (var i = 0; i < data.length; i++) {
+                var countyWithState = `${data[i]["Name"]}, ${data[i]["State Abbreviation"]}`;
+                listOfCounties.push(countyWithState);
+            }
+            return listOfCounties;
+        },
+        removeAllChildNodes(parent) {
+            while (parent.firstChild) {
+                parent.removeChild(parent.firstChild);
+            }
+        },
+        checkIfNodeIsEmpty(node) {
+            if (node.childNodes.length > 0) {
+                return false;
+            }
+            return true;
+        },
+        async readHealthAttribute(clickEvent) {
+
+            this.healthAttribute = clickEvent["target"].value;
+            let csvData = await d3.csv(`../uploads/${this.year}.csv`)
+                .then((data) => {
+                    return data.map((x) => x[this.healthAttribute]);
+                });
+
+            this.healthAttributeData = csvData.map(Number);
+            this.maxValue = Math.max(...this.healthAttributeData);
+            this.minValue = Math.min(...this.healthAttributeData);
+            this.healthAttributeData.sort((a, b) => a - b);
+
+
+        },
+        readCountyCheckbox(clickEvent) {
+
+            if (clickEvent["target"].checked) {
+                var countyAndState = this.parseCountyAndStateName(clickEvent["target"].value);
+                this.selectedCounties.push(clickEvent["target"].value);
+            }
+
+            if (!clickEvent["target"].checked) {
+                let indexOfItemToRemove = this.selectedCounties.indexOf(clickEvent["target"].value);
+                console.log(indexOfItemToRemove);
+
+                // as long as the item is found in the array, continue. 
+                if (indexOfItemToRemove > -1) {
+                    // splice the item from the array to remove it. 
+                    this.selectedCounties.splice(indexOfItemToRemove, indexOfItemToRemove);
+                }
+            }
+        },
+        parseCountyAndStateName(countyState) {
+            var split = countyState.split(",");
+
+            split[0] = split[0].trim();
+            split[1] = split[1].trim();
+
+            return split;
+        }
+    }
+})
 
 export { Chart };
