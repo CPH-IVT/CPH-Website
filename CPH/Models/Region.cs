bbfe1d4e90197a4de6d5296f65c4601e54f9611c
Joshua Trimm <trimmj@etsu.edu>
2022-03-30 16:30:27 -0400

    Update chart and start on regions

---
+++ CPH/Models/Region.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CPH.Models
+{
+    public class Region
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public string FIPS { get; set; } // a string list of county FIPS codes. Ex: 1000,1001,1002,1003.
+    }
+}
