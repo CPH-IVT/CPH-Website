5aedd21c1ebac184dcf7f1e91c9c2c74e3659255
rcstapleton <41754502+rcstapleton@users.noreply.github.com>
2022-04-09 22:07:29 -0400

    DataCleaning1.0

--- CPH/wwwroot/js/uploadCsv.js
+++ CPH/wwwroot/js/uploadCsv.js
@@ -11,7 +11,63 @@
         hashCodeMatch: null,
         fileYearMatch: null,
         uploadSuccess: null,
-        overrideDuplicate: null
+        overrideDuplicate: null,
+        nonCountyBlacklist: [
+            "UNITED STATES",
+            "ALABAMA",
+            "ALASKA",
+            "ARIZONA",
+            "ARKANSAS",
+            "CALIFORNIA",
+            "COLORADO",
+            "CONNECTICUT",
+            "DELAWARE",
+            "DISTRICT OF COLUMBIA",
+            "FLORIDA",
+            "GEORGIA",
+            "HAWAII",
+            "IDAHO",
+            "ILLINOIS",
+            "INDIANA",
+            "IOWA",
+            "KANSAS",
+            "KENTUCKY",
+            "LOUISIANA",
+            "MAINE",
+            "MARSHALL ISLANDS",
+            "MARYLAND",
+            "MASSACHUSETTS",
+            "MICHIGAN",
+            "MINNESOTA",
+            "MISSISSIPPI",
+            "MISSOURI",
+            "MONTANA",
+            "NEBRASKA",
+            "NEVADA",
+            "NEW HAMPSHIRE",
+            "NEW JERSEY",
+            "NEW MEXICO",
+            "NEW YORK",
+            "NORTH CAROLINA",
+            "NORTH DAKOTA",
+            "OHIO",
+            "OKLAHOMA",
+            "OREGON",
+            "PENNSYLVANIA",
+            "RHODE ISLAND",
+            "SOUTH CAROLINA",
+            "SOUTH DAKOTA",
+            "TENNESSEE",
+            "TEXAS",
+            "UTAH",
+            "VERMONT",
+            "VIRGIN ISLANDS",
+            "VIRGINIA",
+            "WASHINGTON",
+            "WEST VIRGINIA",
+            "WISCONSIN",
+            "WYOMING"
+        ]
     },
     methods: {
 
@@ -19,146 +75,71 @@
         *
         * @param {Array} rows
         */
-        removeStates(rows) {
+        removeNonCountyEntriesInDataset(rows) {
+            // Creates an expendable array of state names that is used to identify non-county entries in the dataset
+            let nonCountyBlacklistArray = this.nonCountyBlacklist;
 
-            // Creates an arry of states that is used to remove rows from the dataset
-            let removeCountyListArray = [
-                "UNITED STATES",
-                "ALABAMA",
-                "ALASKA",
-                "ARIZONA",
-                "ARKANSAS",
-                "CALIFORNIA",
-                "COLORADO",
-                "CONNECTICUT",
-                "DELAWARE",
-                "DISTRICT OF COLUMBIA",
-                "FLORIDA",
-                "GEORGIA",
-                "HAWAII",
-                "IDAHO",
-                "ILLINOIS",
-                "INDIANA",
-                "IOWA",
-                "KANSAS",
-                "KENTUCKY",
-                "LOUISIANA",
-                "MAINE",
-                "MARSHALL ISLANDS",
-                "MARYLAND",
-                "MASSACHUSETTS",
-                "MICHIGAN",
-                "MINNESOTA",
-                "MISSISSIPPI",
-                "MISSOURI",
-                "MONTANA",
-                "NEBRASKA",
-                "NEVADA",
-                "NEW HAMPSHIRE",
-                "NEW JERSEY",
-                "NEW MEXICO",
-                "NEW YORK",
-                "NORTH CAROLINA",
-                "NORTH DAKOTA",
-                "OHIO",
-                "OKLAHOMA",
-                "OREGON",
-                "PENNSYLVANIA",
-                "RHODE ISLAND",
-                "SOUTH CAROLINA",
-                "SOUTH DAKOTA",
-                "TENNESSEE",
-                "TEXAS",
-                "UTAH",
-                "VERMONT",
-                "VIRGIN ISLANDS",
-                "VIRGINIA",
-                "WASHINGTON",
-                "WEST VIRGINIA",
-                "WISCONSIN",
-                "WYOMING"
-            ];
-
-            // Creates and populates an array with the indices of the rows to be removed
-            let countiesToRemove = [];
+            // Creates and populates an array with the indices of the rows containing state namnes
+            let indicesOfNonCountiesToRemove = [];
             rows.forEach(function (row) {
-                removeCountyListArray.forEach(function (value) {
+                nonCountyBlacklistArray.forEach(function (value) {
                     if (row.split(",")[4].toUpperCase() === value) {
-                        //console.log(`Removed Row Index: ${rows.indexOf(row)} - County Value: ${row.split(",")[4]}`);
-                        countiesToRemove.push(rows.indexOf(row));
-                        //rows.splice(rows.indexOf(row), rows.indexOf(row));
-                        return
+                        //console.log(`Removed Row Index: ${rows.indexOf(row)} - County Value: ${row.split(",")[4]}`); //Debug Code: Consoles out the indices and values of the rows to be removed.
+                        indicesOfNonCountiesToRemove.push(rows.indexOf(row));
+                        nonCountyBlacklistArray.splice(value, 1);
+                        return;
                     }
                 })
             });
 
             // Removes the rows by indices
-            countiesToRemove.reverse().forEach(function (index) {
-                //console.log(`Removed Row Index: ${index} - County Value: ${rows[index].split(",")[4]}`);
+            indicesOfNonCountiesToRemove.reverse().forEach(function (index) {
+                //console.log(`Removed Row Index: ${index} - County Value: ${rows[index].split(",")[4]}`); ////Debug Code: Consoles out the indices and values of the rows that were removed.
                 rows.splice(index, 1);
             });
-
             // returns the array
+            console.log("State data removed from the county column");
             return rows;
         },
-        /**
-        *
-        * @param {Array} rows
+        /**       
+        *NOTE: Unused at the moment
+        * @param {Array} rows       
         */
         removeBlankRows(rows) {
             // Creates and populates an array with the indices of the rows to be removed
             let blankToRemove = [];
             rows.forEach(function (row) {
                 if (row.split(",")[7] == "") {
-                    //console.log(row.split(",")[7])
                     blankToRemove.push(rows.indexOf(row));
                 }
             });
-
             // Removes the rows by indices
             blankToRemove.reverse().forEach(function (index) {
-                //console.log(`Removed Row Index: ${index} - County Value: ${rows[index].split(",")[7]}`);
+                //console.log(`Removed Row Index: ${index} - County Value: ${rows[index].split(",")[4]}`); //Debug Code: Shows the removed index and county.
                 rows.splice(index, 1);
             });
-            //console.log(`Total Blank Rows Removed: ${blankToRemove.length}`)
+            //console.log(`Total Blank Rows Removed: ${blankToRemove.length}`) //Debug Code: Shows the amount of rows removed
         },
         /**
-        *
+        *NOTE: Unused at the moment
         * @param {Array} rows
         */
-        rowMath(rows) {
-
-            //let dataRows = rows;
-            //dataRows = Array.from(rows);
-            //dataRows.shift();
-
-
-            let dataMean = 0;
-
-            //
-            let columnMean = [];
+        columnMath(rows) {      
+            // Creates and populates a numeric array from a cloumn within the dataset
+            let columnNumericDataArray = [];
             rows.forEach(function (row) {
-
-                //console.log(`Index: ${dataRows.indexOf(row)} - Value: ${dataRows.split(",")[8]}`)
-                //dataMean += parseFloat(dataRows.split(",")[8]);
-
-                columnMean.push(row.split(",")[8]);
+                if (!isNaN(parseFloat(row.split(",")[8]))) {
+                columnNumericDataArray.push(parseFloat(row.split(",")[8]));
+                }              
             });
-
-
-            columnMean.shift();
-            columnMean.forEach(function (value) {
-                dataMean += parseFloat(value);
+            // Sums the data within the array
+            let columnSum = 0;
+            columnNumericDataArray.forEach(function (value) {
+                columnSum += value;
             })
-
-            //
-            console.log(dataMean);
-
-
+            // Consoles out the data results
+            console.log(`Column: ${rows[0].split(",")[8]}\nTotal: ${columnSum}\nMean: ${Math.round((columnSum / columnNumericDataArray.length) * 100) / 100}\nMin: ${Math.min(...columnNumericDataArray)}\nMax: ${Math.max(...columnNumericDataArray)}\nRange: ${Math.max(...columnNumericDataArray) - Math.min(...columnNumericDataArray)}`);
         },
-
-
-
         getSelectedCsv(event) {
             let csvFile = event.target.files[0];
             
@@ -169,43 +150,34 @@
             return csvFile;
         },
         createNewCsv() {
-            if (this.originalCsv !== undefined) {
-
-                this.fileReader.onload = (csvToRead) => {
-                    // get the content of the original csv that the user selected
-                    let contents = csvToRead.target.result;
-
-                    // split the csv on \r rows
-                    let rows = contents.split("\r");
-
-                    // Remove the second row of the CSV
-                    rows.splice(1, 1);
-
-                    // Removes the last row of the CSV
-                    rows.pop();
-
-                    // Removes the state totals from the dataset
-                    this.removeStates(rows);
-
-                    // Removes blank rows from the dataset
-                    this.removeBlankRows(rows);
-
-                    //
-                    //this.rowMath(rows);
-
-                    // Get the year of the file
-                    this.year = rows[2].split(",")[5];
-
-                    // create a csv blob
-                    let csvBlob = new Blob([rows], { type: 'text/csv' });
-
-                    this.alteredCsv = new File([csvBlob], `${this.year}.csv`, { type: "text/csv", lastModified: new Date().getTime() });
-                }
-
-                this.fileReader.readAsBinaryString(this.originalCsv);
-
-                
+            if (this.originalCsv === undefined) {
+                console.log("Internal Error: createNewCsv Failed to load")
             }
+            this.fileReader.onload = (csvToRead) => {
+                // get the content of the original csv that the user selected
+                let contents = csvToRead.target.result;
+
+                // split the csv on \r rows
+                let rows = contents.split("\r");
+
+                // Remove the second row of the CSV
+                rows.splice(1, 1);
+
+                // Removes the last row of the CSV
+                rows.pop();
+
+                // Removes the state totals from the dataset
+                this.removeNonCountyEntriesInDataset(rows);
+
+                // Get the year of the file
+                this.year = rows[2].split(",")[5];
+
+                // create a csv blob
+                let csvBlob = new Blob([rows], { type: 'text/csv' });
+
+                this.alteredCsv = new File([csvBlob], `${this.year}.csv`, { type: "text/csv", lastModified: new Date().getTime() });
+            }
+            this.fileReader.readAsBinaryString(this.originalCsv);                   
         },
         setAlteredCsv(event) {
             this.originalCsv = this.getSelectedCsv(event);
